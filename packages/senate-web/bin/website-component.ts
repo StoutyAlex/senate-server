f9da874ee448f0b65256e723fb379a749b55d6cd
StoutyAlex <alexstout009@hotmail.com>
2022-03-11 15:48:00 +0000

    new cert

--- packages/senate-web/bin/website-component.ts
+++ packages/senate-web/bin/website-component.ts
@@ -6,7 +6,7 @@ const app = new App()
 const config: SenateWebsiteProps = {
     domainName: 'senate-server.co.uk',
     bucketName: 'senate-server-website-frontend',
-    certificateArn: 'arn:aws:acm:eu-west-1:477948800870:certificate/86984a38-25e9-4da4-9400-847ad87d023a',
+    certificateArn: 'arn:aws:acm:eu-west-1:477948800870:certificate/234eb11e-59d3-4acb-abac-4753d376fd22',
     hostedZone: {
         id: 'Z031958332WIF5IG4KX9O',
         name: 'senateserver.co.uk'
